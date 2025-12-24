11aebe18ad96a6267cb6bc6dad89fb936599bfd0
Naufal54 <naufal.alfath5@gmail.com>
2025-12-24 17:11:37 +0700

    update fixed orintation (portrait) and simplify file

--- fall_detection_app/lib/main.dart
+++ fall_detection_app/lib/main.dart
@@ -1,13 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:eldercare/app/app.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-    DeviceOrientation.portraitDown,
-  ]);
+void main() {
   runApp(const MyApp());
 }
   
