d8427593d1450555bd9b24788594802d28f62a54
Naufal54 <naufal.alfath5@gmail.com>
2025-12-24 18:06:50 +0700

    change folder structure

---
+++ fall_detection_app/lib/app/routes.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:eldercare/features/emergency/emergency_page.dart';
+import 'package:eldercare/features/home/home_page.dart';
+import 'package:eldercare/features/profile/profile_page.dart';
+
+class AppRoutes {
+  static const home = '/';
+  static const emergency = '/emergency';
+  static const profile = '/profile';
+
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case home:
+        return MaterialPageRoute(builder: (_) => const HomePage());
+      case emergency:
+        return MaterialPageRoute(builder: (_) => const EmergencyPage());
+      case profile:
+        return MaterialPageRoute(builder: (_) => const ProfilePage());
+      default:
+        return MaterialPageRoute(
+          builder: (_) => const Scaffold(
+            body: Center(child: Text('Route not found')),
+          ),
+        );
+    }
+  }
+}
